2838e8eb18e8ff37218b2e86b9a9f34212576136
Joelle Helm <joellenhelm@gmail.com>
2019-08-28 15:37:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,12 +12,6 @@ end
 
 def find_the_cheese(cheese_arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_arr.find do |word|
-    if cheese_types.include?(word)
-      return word
-    else
-      return nil
-  end
-end
+  cheese_arr.find { |word| cheese_types.include?(word) }
 
 end
